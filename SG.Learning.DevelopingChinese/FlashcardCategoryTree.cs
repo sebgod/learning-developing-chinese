89bd4914009c36697f772837ddf2473dca39d5f1
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-11-03 21:05:44 +0800

    FlashcardCategoryTree
    FlashcardCategoryTree now supports adding of new categories, by updating
    the XDocument accordingly

--- SG.Learning.DevelopingChinese/FlashcardCategoryTree.cs
+++ SG.Learning.DevelopingChinese/FlashcardCategoryTree.cs
@@ -8,19 +8,39 @@ namespace SG.Learning.DevelopingChinese
 {
     public class FlashcardCategoryTree
     {
-        private readonly HashSet<string> _categorySet;
+        private readonly XContainer _categoryRoot;
+        private readonly Dictionary<string, XElement> _categoryMap;
         private readonly List<string> _categoryList;
 
-        public FlashcardCategoryTree(XContainer catRoot)
+        public FlashcardCategoryTree(XContainer categoryRoot)
         {
-            if (catRoot == null)
+            if (categoryRoot == null)
             {
                 throw new ArgumentException(
                     Resources.FlashcardCategoryTree_FlashcardCategoryTree_The_flashcard_list_contains_no_categories,
-                    "catRoot");
+                    "categoryRoot");
             }
-            _categoryList = catRoot.Elements("category").Select(pCat => pCat.Attribute("name").Value).ToList();
-            _categorySet = new HashSet<string>(_categoryList);
+            _categoryRoot = categoryRoot;
+            var categoryElements = categoryRoot.Elements("category").ToList();
+            Func<XElement, string> keySelector = pCat => pCat.Attribute("name").Value;
+            _categoryList = categoryElements.Select(keySelector).ToList();
+            _categoryMap = categoryElements.ToDictionary(keySelector);
+        }
+
+        public bool Add(params string[] subCategories)
+        {
+            var catFQN = JoinSubCategories(subCategories);
+            var alreadyInSet = _categoryMap.ContainsKey(catFQN);
+            if (!alreadyInSet)
+            {
+                _categoryList.Add(catFQN);
+                var catNode = new XElement("category");
+                catNode.SetAttributeValue("name", catFQN);
+                _categoryRoot.Add(catNode);
+                _categoryMap[catFQN] = catNode;
+                return true;
+            }
+            return false;
         }
 
         public bool ContainsCategory(params string[] subCategories)
@@ -30,7 +50,12 @@ namespace SG.Learning.DevelopingChinese
                     Resources.FlashcardCategoryTree_ContainsCategory_Please_specify_at_least_one_category,
                     "subCategories");
 
-            return _categorySet.Contains(string.Join("/", subCategories));
+            return _categoryMap.ContainsKey(JoinSubCategories(subCategories));
+        }
+
+        private static string JoinSubCategories(string[] subCategories)
+        {
+            return string.Join("/", subCategories);
         }
     }
 }
